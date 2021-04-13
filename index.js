9209c40a2480650f98c9d4c4c4a7aa4c14f6c427
Molly White <molly.white5@gmail.com>
2021-04-12 20:35:18 -0400

    CORS

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+
 const PORT = process.env.PORT || 5000;
 
 const paginated = require('./api/utils/middleware');
 const api = require('./api/api');
 
 const app = express();
+app.use(cors());
 app.all('*', paginated);
 app.use('/api', api);
 app.listen(PORT);
