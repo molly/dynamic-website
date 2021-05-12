acd41045a834d529585d0bc0bdac11bc8927fef9
Molly White <molly.white5@gmail.com>
2021-05-11 22:28:48 -0400

    + summary

--- data/filter/rss.js
+++ data/filter/rss.js
@@ -9,6 +9,44 @@ const { makeSortBySimpleDateKey } = require('../utils/dateUtils');
 
 const DIB_DEFAULTS = require('../dibDefaults');
 
+// Trim the summary to length characters without splitting words, then append ellipses
+const getSummaryExcerpt = (summary, length) => {
+  if (summary.length <= length) {
+    return summary;
+  }
+  const trimmed = summary.substr(0, summary.lastIndexOf(' ', length));
+  return trimmed + '...';
+};
+
+const makeSummary = (article) => {
+  let summary = `"${article.title}"`;
+  if (article.parenthetical) {
+    summary += ` (${article.parenthetical})`;
+  }
+
+  if (article.parenthetical || !article.title.match(/[.?!]$/m)) {
+    summary += '. ';
+  } else {
+    summary += ' ';
+  }
+
+  if (article.author) {
+    summary += article.author;
+  }
+  if (article.work || article.publisher) {
+    summary += ` ${article.preposition} ${article.work || article.publisher}`;
+  }
+
+  if (article.formattedDate) {
+    summary += ` on ${article.formattedDate}.`;
+  } else if (article.author || article.work || article.publisher) {
+    summary += '.';
+  }
+  const textOnlySummary = article.summary.replace(/(<([^>]+)>)/gi, '');
+  const trimmedSummary = getSummaryExcerpt(textOnlySummary, 200);
+  return summary + ' ' + trimmedSummary;
+};
+
 const getRssResults = async () => {
   const dibPugTemplate = pug.compileFile(
     path.join(__dirname, '../../pug/etc/rssDibArticle.pug')
@@ -20,6 +58,7 @@ const getRssResults = async () => {
     article.entryHtml = dibPugTemplate({
       article,
     });
+    article.rssSummary = makeSummary(article);
     if (!article.entryAdded) {
       article.entryAdded = getMomentFromWeek(article.week).toISOString();
     } else {
