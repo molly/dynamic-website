254ade33bd910f5976a139347729aca721a40e23
Molly White <molly.white5@gmail.com>
2022-09-12 22:22:40 -0400

    Update or add tags on save

--- backend/routes/reading-list.js
+++ backend/routes/reading-list.js
@@ -20,13 +20,37 @@ const models = {
   shortform: ShortformEntry,
 };
 
+const tagModels = {
+  blockchain: BlockchainTag,
+  press: PressTag,
+  shortform: ShortformTag,
+};
+
+const updateTags = async (type, entry) => {
+  if (entry.tags && entry.tags.length) {
+    const TagModel = tagModels[type];
+    for (const tag of entry.tags) {
+      let tagRecord = await TagModel.findOne({ value: tag });
+      if (tagRecord) {
+        tagRecord.frequency += 1;
+      } else {
+        tagRecord = new TagModel({
+          value: tag,
+          text: tag.toLowerCase().replace(/_/g, ' '),
+          frequency: 1,
+        });
+      }
+      await tagRecord.save();
+    }
+  }
+};
+
 const getCollectionTags = async (model) => {
   const tags = await model.find({}, '-_id');
   return sortBy(tags, (t) => t.text.toLowerCase());
 };
 
 router.get('/tags', async (_, res) => {
-  // TODO: Cache this
   const tags = {
     blockchain: await getCollectionTags(BlockchainTag),
     press: await getCollectionTags(PressTag),
@@ -40,6 +64,7 @@ router.post('/entry', verifyJwt, async (req, res) => {
   const model = new models[type](entry);
   try {
     await model.save();
+    await updateTags(type, entry);
     return res.status(204).send();
   } catch (err) {
     return res.status(400).send({ message: err });
