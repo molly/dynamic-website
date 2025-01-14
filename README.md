3d12d3fcbaaf1c87798d2e72002962ce5974bdac
Molly White <molly.white5@gmail.com>
2025-01-14 15:03:43 -0500

    rm, haven't been keeping this updated

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Small app to power the dynamic portions of my website:
 
+- [activity feed](https://www.mollywhite.net/feed)
 - [reading lists](https://www.mollywhite.net/reading)
 - [press page](https://www.mollywhite.net/press)
-- [wikipedia articles](https://www.mollywhite.net/wikipedia-work)
 
 The static site files can be found at [website-v3](https://github.com/molly/website-v3).
--- frontend/routes/frontendRouter.js
+++ frontend/routes/frontendRouter.js
@@ -4,14 +4,12 @@ import homeRouter from './homeRouter.js';
 import microRouter from './microRouter.js';
 import pressRouter from './pressRouter.js';
 import readingListRouter from './readingList.js';
-import wikipediaRouter from './wikipediaRouter.js';
 
 const app = express.Router();
 
 app.use('/', homeRouter);
 app.use('/reading', readingListRouter);
 app.use('/press', pressRouter);
-app.use('/wikipedia-work', wikipediaRouter);
 app.use('/micro', microRouter);
 app.use('/feed', feedRouter);
 
